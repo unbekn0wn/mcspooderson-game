5f412640b6356f2e9dedc3bc07ccde7fdea8f889
Arno Flapper <flapperarno@gmail.com>
2019-09-26 16:24:51 +0200

    First commit

---
+++ McSpooderson/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed;
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            Vector3 desiredDirection = Vector3.Normalize(new Vector3(Camera.main.transform.forward.x, 0, Camera.main.transform.forward.z));
+            transform.rotation = Quaternion.LookRotation(desiredDirection);
+            transform.position += transform.forward * movementSpeed * Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log(transform.forward);
+        }
+    }
+}
